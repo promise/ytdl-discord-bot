b0a9d134e0f3a7b4bed6b9f729720519247236e2
Promise <glelud@gmail.com>
2020-07-23 01:50:31 +0200

    Queue command no longer shows current song in list of queued songs

--- commands/queue.js
+++ commands/queue.js
@@ -2,11 +2,12 @@ module.exports.permissionRequired = 0
 
 module.exports.run = async (client, message, args, config, queue) => {
   const serverQueue = queue.get(message.guild.id)
-  if (!serverQueue) return message.channel.send("❌ There is nothing playing right now!")
+  if (!serverQueue || serverQueue.songs.length == 0) return message.channel.send("❌ There is nothing playing right now!")
+  if (serverQueue.songs.length == 1) return message.channel.send("❌ The queue is empty!")
 
   return message.channel.send([
     "__**Song queue:**__",
-    serverQueue.songs.map(song => "- " + song.title).join("\n"),
+    serverQueue.songs.slice(1).map(song => "- " + song.title).join("\n"),
     "**Now playing:** " + serverQueue.songs[0].title
   ].join("\n\n"))
 }
