1d2a225f73124edf5f59aad38e090280930d3633
Promise <glelud@gmail.com>
2020-07-24 19:09:45 +0200

    Convert string to template string

--- commands/np.js
+++ commands/np.js
@@ -4,5 +4,5 @@ module.exports.run = async (client, message, args, config, queue) => {
   const serverQueue = queue.get(message.guild.id)
   if (!serverQueue) return message.channel.send("❌ There is nothing playing right now!")
 
-  return message.channel.send("🎶 Now playing **" + serverQueue.songs[0].title + "**")
+  return message.channel.send(`🎶 Now playing **${serverQueue.songs[0].title}**`)
 }
